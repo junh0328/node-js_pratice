4e2efeb17be136b03b56be5864e6cd84b7b084da
junh0328 <junh0328@naver.com>
2020-08-29 17:19:09 +0900

    node_02.js 추가 socket.io를 통한 로컬 연결 및 테스트

---
+++ Day10/node_03.js
@@ -0,0 +1,87 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const static=require('serve-static');
+const path=require('path');
+const logger=require('morgan');
+const expressErrorHandler=require('express-error-handler');
+const cookieParser=require('cookie-parser');
+const expressSession=require('express-session');
+const passport=require('passport');
+//npm install socket.io 
+/* 서버용 소켓? 사용자를 위해 대기,  클라이언트용 소켓? 사용자들끼리 소통하기 위함 */
+const socketio = require('socket.io');
+//npm install cors
+// 클라이언트에서 ajax로 요청시 cors(다중 서버 접속)을 지원하겠다.
+const cors = require('cors');
+
+let app=express();
+let port=3000;
+let router=express.Router();
+let router_loader=require('./routes/route_loader');
+router_loader.init(app, router);
+
+app.use(cookieParser());
+app.use(expressSession({
+    secret:'!@#$%^&*()',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{maxAge:60*60*1000}
+}));
+
+// 클라이언트에서 ajax로 요청시 cors 지원을 익스프레스에 등록한다.
+app.use(cors());
+
+// passport 사용설정
+// passport의 세션을 사용하려면 그 전에 Express의 세션을 사용하는 코드가 있어야 한다.
+app.use(passport.initialize()); // initialize() 함수롤 호출하면 사용할 수 있다.
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended:false}));
+// /public이라는 명령어를 치면 현재디렉토리와 'public' 폴더를 연결합니다.
+app.use('/public', static(path.join(__dirname, 'public')));
+// loger('dev') : 요청에 대한 정보를 콘솔에 기록해줍니다.
+app.use(logger('dev'));
+app.use('/', router);
+
+let errorHandler=expressErrorHandler({
+    static:{
+        '404':'./public/404.html'
+    }
+});
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+/* ejs 를 사용한 처리 */ // 현재 디렉토리에 views 라는 폴더를 추가한다.
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+
+/* db 설정 파일 불러오기 */
+// 몽고디비를 연결한 주소와, 컬렉션, 스키마에 대한 정의를 하는 곳이다.
+let config = require('./config/config');
+
+/* db를 만들어 연결*/
+let database = require('./database/database');
+
+/* 패스포트 설정 (로그인, 계정 담당) */
+let configPassport = require('./config/passport');
+configPassport(app, passport);
+
+
+let userPassport=require('./routes/route_member');
+userPassport(router, passport);
+
+const server = app.listen(port, ()=>{
+    console.log('Server listening on port : ' + config.server_port);
+    // database.init() : 서버가 실행되면서 db를 연결해준다.
+    database.init(app, config);
+});
+
+//socket.io 서버
+const io = socketio.listen(server);
+console.log('socket.io 요청을 받을 준비 완료!');
+
+io.sockets.on('connection', (socket)=>{
+    //peername : 접속자의 이름
+    console.log('connection : ', socket.request.connection._peername);
+    socket.remoteAddress = socket.request.connection._peername.address;
+    socket.remotePort = socket.request.connection._peername.port;
+});
---
+++ Day10/readme.md
@@ -0,0 +1,4 @@
+- node_01.js 까지 facebook을 통한 패스포트 연결 및 로그인
+
+- node_02.js 부터 socket.io 를 이용한 실시간 채팅 서비스 제공
+- cors를 사용하기 위해서 express 서버에 등록해서 사용
